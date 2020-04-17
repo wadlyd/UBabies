bdfb30635327fa50b5d2664f6e645cbb88894706
Douv <dukyw1@gmail.com>
2020-04-16 22:21:17 -0300

    first commit

---
+++ www/js/my-app.js
@@ -0,0 +1,114 @@
+  
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      {
+        path: '/index/',
+        url: 'index.html',
+      },
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+      {
+        path: '/panel/',
+        url: '/panel.html/',
+      },
+      {
+        path: '/registro/',
+        url: 'registrarse.html',
+      },
+      {
+        path: '/firstlog/',
+        url: 'firstlog.html',
+      },
+
+    ]
+    // ... other parameters
+  });
+
+var mainView = app.views.create('.view-main');
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+
+    $$('#inicio').on('click', fnSetEmail);
+
+});
+
+
+
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+    // Do something here when page loaded and initialized
+    console.log(e);
+})
+
+
+
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="registro"]', function (e) {
+    // Inicio Panel
+    console.log(e);
+
+    $$('#inicio').on('click', fnSetEmail);
+})
+
+
+/*$$(document).on('page:init', '.page[data-name="panel"]', function(e){
+    //Inicio Panel
+    console.log(e);
+})*/
+
+
+/** FUNCIONES PROPIAS **/
+function fnSetEmail() {
+    var elMail = $$('#email').val(); //recupero el valor del input mail
+
+    //lo seteo en el panel del menu
+    $$('#labelEmail').text(elMail);
+
+    idDelBoton = this.id;
+    mensaje = "Mi ID es: " + idDelBoton + "<br/>";
+
+    // voy a "separar" el valor del id, usando los guiones bajos
+    // el split separa un valor (en este caso una variable),
+    // usando el caracter o caracteres indicandos como parámetro
+    // el resultado es un array!
+    var partes = idDelBoton.split("_");
+
+    // sabiendo la forma: btn_g1_1 puedo tener:
+    p0 = partes[0];
+    p1 = partes[1];
+    p2 = partes[2];
+
+    mensaje += "Soy del Grupo: " + p1 + "<br/>";
+    mensaje += "Y tengo el nro: " + p2 + "<br/>";
+
+
+    $$('#msgBtn').html(mensaje);
+
+
+
+}
+
+
+
